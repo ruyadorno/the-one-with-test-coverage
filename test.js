114dd3c07d31dcd652b6686f21997d30b548d7e2
Ruy Adorno <ruyadorno@hotmail.com>
2019-10-08 16:33:31 -0400

    Added patch global api example

--- test.js
+++ test.js
@@ -9,4 +9,17 @@ describe('App', () => {
   it('should render not found message', () => {
     expect(notFound()).toEqual('404 - Not Found\n')
   })
+
+  it('should log req.url', () => {
+    expect.assertions(2)
+
+    const consoleLog = console.log
+    console.log = (prefix, msg) => {
+      expect(prefix + msg).toEqual('info: /foo')
+    }
+
+    expect(log({
+      url: '/foo'
+    })).toEqual('')
+  })
 })
